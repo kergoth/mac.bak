5df8f9bcad0a576fc4a9245c6f6344c5f35b1507
Christopher Larson <kergoth@gmail.com>
2020-12-01 12:20:26 -0700

    Alter cli argument parsing to handle multiple apps
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- mac.bak.sh
+++ mac.bak.sh
@@ -6,16 +6,14 @@
 
 # Run backups to whatever directory you specify
 
-current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 function usage() {
     echo "Usage:"
     echo "mac.bak.sh "
     echo "mac.bak.sh -h "
-    echo "mac.bak.sh --dir=~/backups "
-    echo "mac.bak.sh --app=firefox "
+    echo "mac.bak.sh [--dir=~/backups] [APP_NAME..] "
     echo ""
-    echo "   -a | --app         specify just a single app to backup"
     echo "   -d | --dir         the backup directory"
     echo "   --dry-run          rsync dry run rather than backup"
     echo "   --delete-excluded  rsync delete excluded"
@@ -24,7 +22,6 @@ function usage() {
 
 # vars
 backupdir="${current_dir}/backups"
-app_name=
 app_dir="${current_dir}/apps"
 custom_dir="${current_dir}/custom"
 rsync_combined="${current_dir}/include-from.rsync"
@@ -34,42 +31,60 @@ delete_excluded=
 # parse command args
 while [ "$1" != "" ]; do
     case $1 in
-        -d | --dir )         shift
-                             backupdir=${1%/}
-                             ;;
-        -a | --app )         shift
-                             app_name=$1
-                             ;;
-        --dry-run )          dry_run=1
-                             ;;
-        --delete-excluded )  delete_excluded=1
-                             ;;
-        -h | --help )        usage
-                             exit
-                             ;;
-        * )                  usage
-                             exit 1
+        --)
+            shift
+            break
+            ;;
+        -d | --dir)
+            shift
+            backupdir=${1%/}
+            shift
+            ;;
+        --dry-run)
+            dry_run=1
+            shift
+            ;;
+        --delete-excluded)
+            delete_excluded=1
+            shift
+            ;;
+        -*)
+            usage
+            exit
+            ;;
+        *)
+            break
+            ;;
     esac
-    shift
 done
 
-if [[ -z $app_name ]]; then
+if [ $# -eq 0 ]; then
     echo "Backing up everything to ${backupdir}"
+    app_msg=0
+    # shellcheck disable=SC2046
+    set -- $(find "$app_dir" -type f -a \( -name \*.rsync -o -name backup.sh \) | sed -e 's#/[^/]*$##; s#.*/##; s#\.cfg$##')
 else
-    echo "Backing up ${app_name} to ${backupdir}"
-    app_dir="${app_dir}/${app_name}"
+    app_msg=1
 fi
 
-# first run all backup.sh scripts
-if [[ -z $dry_run ]]; then
-    find "${app_dir}" -type f -name 'backup.sh' -exec {} \;
-else
-    echo "skipping backup.sh scripts for dry-run"
-fi
+rm -f "${rsync_combined}.tmp"
+for app_name; do
+    if [[ $app_msg -eq 1 ]]; then
+        echo "Backing up ${app_name} to ${backupdir}"
+    fi
+    this_app_dir="${app_dir}/${app_name}"
 
-# get all rsync instructions files into one
-find "${app_dir}" -type f -name 'include-from.rsync' -exec cat {} \; > "${rsync_combined}.tmp"
-find "${custom_dir}" -type f -name 'include-from.rsync' -exec cat {} \; >> "${rsync_combined}.tmp"
+    # first run all backup.sh scripts
+    if [[ -z $dry_run ]]; then
+        find "${this_app_dir}" -type f -name 'backup.sh' -exec {} \;
+    else
+        echo "skipping backup.sh scripts for dry-run"
+    fi
+
+    # get all rsync instructions files into one
+    find "${this_app_dir}" -type f -name 'include-from.rsync' -exec cat {} \; >>"${rsync_combined}.tmp"
+    find "${custom_dir}" -type f -name 'include-from.rsync' -exec cat {} \; >>"${rsync_combined}.tmp"
+done
 
 # `awk '!/[^\#]/ || !seen[$0]++'` is a uniq trick without the need for a sort
 cat "${rsync_combined}.tmp" | awk '!/[^\#]/ || !seen[$0]++' > "${rsync_combined}"
@@ -77,10 +92,10 @@ rm "${rsync_combined}.tmp"
 
 # rsync
 rsync_cmd="rsync -acvL "
-if [[ ! -z $dry_run ]]; then
+if [[ -n $dry_run ]]; then
     rsync_cmd+="--dry-run "
 fi
-if [[ ! -z $delete_excluded ]]; then
+if [[ -n $delete_excluded ]]; then
     rsync_cmd+="--delete-excluded "
 fi
 rsync_cmd+="--include-from=\"${current_dir}/header.rsync\" "
